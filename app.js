3fda96ee6686c36b25f680222461b255a00910fa
Leonardo Rodrigues <leonardo.rxs@hotmail.com>
2018-03-07 19:47:51 -0300

    Adding start file

--- app.js
+++ app.js
@@ -164,6 +164,6 @@ app.get("*", function(req, res) {
 });
 
 // ================ START SERVER ===============
-app.listen(process.env.PORT || 3000, function(){
+app.listen(3000, function(){
   console.log("The YelpCamp Server has started!");
 });
