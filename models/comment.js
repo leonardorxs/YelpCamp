ad46be958c31ee3a8c9a0ca4ac032b6e3d1cbf35
Leonardo Rodrigues <leonardo.rxs@hotmail.com>
2018-03-22 01:07:38 -0300

    Harp + Pages Commit

--- models/comment.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
- 
-var commentSchema = mongoose.Schema({
-    text: String,
-    author: String,
-    date: { type: Date, default: Date.now }
-});
- 
-module.exports = mongoose.model("Comment", commentSchema);
